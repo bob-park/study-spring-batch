d42e59d43f47c6a2db7fb5e46722e2b85ba1748b
Bob Park <hwpark@malgn.com>
2021-11-11 19:57:19 +0900

    feat : JobParameter

--- spring-batch/src/main/java/com/example/springbatch/configure/JobParameterConfiguration.java
+++ spring-batch/src/main/java/com/example/springbatch/configure/JobParameterConfiguration.java
@@ -1,18 +1,21 @@
 package com.example.springbatch.configure;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class JobInstanceConfiguration {
+public class JobParameterConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -27,8 +30,22 @@ public class JobInstanceConfiguration {
 
     @Bean
     public Step step1() {
+        // Job Parameter 는 Step 단계에서 참조할 수 있다.
         return stepBuilderFactory.get("step1")
             .tasklet((contribution, chunkContext) -> {
+
+                // contribution 으로 job parameter 가져오기 - 실사용시
+                JobParameters jobParameters = contribution.getStepExecution().getJobParameters();
+
+                jobParameters.getString("name");
+                jobParameters.getLong("seq");
+                jobParameters.getDate("date");
+                jobParameters.getDouble("age");
+
+                // chunk context 로 job parameter 가져오기 - 값 확인용
+                Map<String, Object> jobParamMap = chunkContext.getStepContext()
+                    .getJobParameters();
+
                 log.info("step1 was execute");
                 return RepeatStatus.FINISHED;
             })
--- spring-batch/src/main/java/com/example/springbatch/runner/JobParameterRunner.java
+++ spring-batch/src/main/java/com/example/springbatch/runner/JobParameterRunner.java
@@ -1,5 +1,6 @@
 package com.example.springbatch.runner;
 
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -7,21 +8,11 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
-/**
- *
- * ! 주의사항
- *
- * <pre>
- *      - 같은 Job + Job Parameters 가 같은 경우 같은 Job Instance 가 반환된다.
- *      - 이미 실행된 Job Instance 는 다시 실행되지 않는다.
- * </pre>
- *
- *
- */
-//@Component
+@Component
 @RequiredArgsConstructor
-public class JobRunner implements ApplicationRunner {
+public class JobParameterRunner implements ApplicationRunner {
 
     private final JobLauncher jobLauncher;
     private final Job job;
@@ -31,8 +22,12 @@ public class JobRunner implements ApplicationRunner {
 
         JobParameters jobParameters = new JobParametersBuilder()
             .addString("name", "user1")
+            .addLong("seq", 2L)
+            .addDate("date", new Date())
+            .addDouble("age", 16.5)
             .toJobParameters();
 
         jobLauncher.run(job, jobParameters);
+
     }
 }
