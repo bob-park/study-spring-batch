00e54cc7f1db2096a26f051f5d351d38129128cd
Bob Park <hwpark11@kakao.com>
2021-11-15 15:20:18 +0900

    feat : SimpleJob - start() / end()

--- spring-batch-job/src/main/java/com/example/springbatchjob/ch03_simplejob/SimpleJobConfiguration.java
+++ spring-batch-job/src/main/java/com/example/springbatchjob/ch03_simplejob/SimpleJobConfiguration.java
@@ -27,7 +27,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class SimpleJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
