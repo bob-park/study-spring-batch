6e71431594203bd4b765131cdd703b57ed58f249
Bob Park <hwpark11@kakao.com>
2021-11-15 16:19:00 +0900

    feat : SimpleJob Architecture

--- spring-batch-job/src/main/java/com/example/springbatchjob/ch03_simplejob/SimpleJobArchitectureConfiguration.java
+++ spring-batch-job/src/main/java/com/example/springbatchjob/ch03_simplejob/SimpleJobArchitectureConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.springbatchjob.ch03_simplejob;
 
-import com.example.springbatchjob.ch03_simplejob.incrementer.CustomJobParametersIncrementer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -13,18 +12,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * SimpleJob incrementer()
+ * SimpleJob Architecture
  *
  * <pre>
- *      - JobParameters 에서 필요한 값을 증가시켜서 다음에 사용될 JobParameters Object 를 return
- *      - 기존의 JobParameter 변경없이 Job 을 여러번 시작하고자 할떄
- *      - RunIdIncrementer 구현체를 지원하여, Interface 를 직접 구현할 수 있음
  * </pre>
  */
 @Slf4j
 @RequiredArgsConstructor
-//@Configuration
-public class IncrementerConfiguration {
+@Configuration
+public class SimpleJobArchitectureConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -32,11 +28,9 @@ public class IncrementerConfiguration {
     @Bean
     public Job batchJob1() {
         return jobBuilderFactory.get("batchJob1")
+            .incrementer(new RunIdIncrementer())
             .start(step1())
             .next(step2())
-            .next(step3())
-//            .incrementer(new CustomJobParametersIncrementer()) // custom
-            .incrementer(new RunIdIncrementer()) // default
             .build();
     }
 
@@ -60,14 +54,5 @@ public class IncrementerConfiguration {
             .build();
     }
 
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-            .tasklet((contribution, chunkContext) -> {
-                log.info("step3 was execute");
-                return RepeatStatus.FINISHED;
-            })
-            .build();
-    }
 
 }
