be784910f05309e9209063d2086a1062358d0894
Bob Park <hwpark11@kakao.com>
2021-11-15 15:52:47 +0900

    feat : SimpleJob - validator()

--- spring-batch-job/src/main/java/com/example/springbatchjob/ch03_simplejob/SimpleJobValidatorConfiguration.java
+++ spring-batch-job/src/main/java/com/example/springbatchjob/ch03_simplejob/SimpleJobValidatorConfiguration.java
@@ -1,41 +1,47 @@
 package com.example.springbatchjob.ch03_simplejob;
 
+import com.example.springbatchjob.ch03_simplejob.validator.CustomJobParametersValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * SimpleJob start() / next()
+ * SimpleJob Validator
  *
  * <pre>
- *     - start()
- *          - 처음 실행할 Step 설정, 최초 한번 설정, SimpleJobBuilder 가 생성되고 반환된다.
- *     - next()
- *          - 다음 실행할 Step 을 순차적으로 연결 설정
- *          - 여러번 설정 가능
- *          - 모든 next() 의 Step 이 종료가 되면 Job 종료
+ *      - Job 실행에 꼭 필요한 파라미터를 검증하는 용도
+ *      - DefaultJobParametersValidator 구현체를 지원
+ *      - 좀 더 복잡한 제약 조건이 있다면, Interface 를 직접 구현할 수 있음
  * </pre>
- *
  */
 @Slf4j
 @RequiredArgsConstructor
-//@Configuration
-public class SimpleJobAPIConfiguration {
+@Configuration
+public class SimpleJobValidatorConfiguration {
+
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job job1() {
-        return jobBuilderFactory.get("job1")
+    public Job batchJob1() {
+        return jobBuilderFactory.get("batchJob1")
             .start(step1())
             .next(step2())
             .next(step3())
+            // Repository 에서 생성하기 전 실행, job 실행하기 전 실행, 총 2번 실행
+//            .validator(new CustomJobParametersValidator()) // Custom
+            .validator(new DefaultJobParametersValidator(
+                // required
+                new String[]{"name", "date"},
+                // optional
+                new String[]{"count"})) // default
             .build();
     }
 
