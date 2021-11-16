12150f99f3d51b8bb93e3cba2d6e4d3f170ba043
Bob Park <hwpark11@kakao.com>
2021-11-16 12:44:09 +0900

    feat : Initialize project

---
+++ spring-batch-step/src/main/java/com/example/springbatchstep/SpringBatchStepApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchStepApplication.class, args);
+    }
+
+}
---
+++ spring-batch-step/src/test/java/com/example/springbatchstep/SpringBatchStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
