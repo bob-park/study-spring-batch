a98b4afe9175b57d854f83dca7b44b87f7641393
Bob Park <hwpark11@kakao.com>
2021-11-16 14:11:16 +0900

    feat : TaskletStep Architecture

--- spring-batch-step/src/main/java/com/example/springbatchstep/ch02_taskletstep/TaskletArchitectureConfiguration.java
+++ spring-batch-step/src/main/java/com/example/springbatchstep/ch02_taskletstep/TaskletArchitectureConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.springbatchstep.ch02_taskletstep;
 
-import com.example.springbatchstep.ch02_taskletstep.tasklet.CustomTasklet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -13,27 +12,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * TaskletStep - startLimit() / allowStartIfComplete()
+ * TaskletStep Architecture
  *
  * <pre>
- *     - startLimit()
- *          - Step 의 실행 횟수를 조정할 수 있다.
- *          - Step 마다 설정할 수 있다.
- *          - 설정값을 초과해서 다시 실행하려고 하면, StartLimitExceededException 발생
- *          - start-limit 의 default : Integer.MAX_VALUE
- *
- *     - allowStartIfComplete()
- *          - 재시작 가능한 job 에서 Step 의 이전 성공 여부와 상관없이 항상 Step 을 실행하기 위한 설정
- *          - 실행마다 유효성을 검증하는 Step 이나 사전 작업이 꼭 필요한 Step 등
- *          - 기본적으로 Completed 상태를 가진 Step 은 Job 재시작 시 실행하지 않고 Skip 한다.
- *          - allow-start-if-completed 가 true 로 설정된 step 은 항상 실행
+ *     -
  * </pre>
- *
  */
 @Slf4j
 @RequiredArgsConstructor
-//@Configuration
-public class TaskletExtraConfiguration {
+@Configuration
+public class TaskletArchitectureConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -41,7 +29,7 @@ public class TaskletExtraConfiguration {
     @Bean
     public Job batchJob1() {
         return jobBuilderFactory.get("batchJob1")
-//            .incrementer(new RunIdIncrementer())
+            .incrementer(new RunIdIncrementer())
             .start(step1())
             .next(step2())
             .build();
@@ -54,7 +42,6 @@ public class TaskletExtraConfiguration {
                 log.info("step1 was execute");
                 return RepeatStatus.FINISHED;
             })
-            .allowStartIfComplete(true)
             .build();
     }
 
@@ -63,10 +50,8 @@ public class TaskletExtraConfiguration {
         return stepBuilderFactory.get("step2")
             .tasklet((contribution, chunkContext) -> {
                 log.info("step2 was execute");
-                throw new RuntimeException("step2 failed.");
-//                return RepeatStatus.FINISHED;
+                return RepeatStatus.FINISHED;
             })
-            .startLimit(3)
             .build();
     }
 
