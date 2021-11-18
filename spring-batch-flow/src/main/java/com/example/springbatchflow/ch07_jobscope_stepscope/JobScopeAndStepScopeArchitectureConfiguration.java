362502b94fda77c9cef94123ad36f623a5d63bc9
Bob Park <hwpark11@kakao.com>
2021-11-18 22:36:43 +0900

    feat : JopScope / StepScope - Architecture

--- spring-batch-flow/src/main/java/com/example/springbatchflow/ch07_jobscope_stepscope/JobScopeAndStepScopeArchitectureConfiguration.java
+++ spring-batch-flow/src/main/java/com/example/springbatchflow/ch07_jobscope_stepscope/JobScopeAndStepScopeArchitectureConfiguration.java
@@ -2,7 +2,6 @@ package com.example.springbatchflow.ch07_jobscope_stepscope;
 
 import com.example.springbatchflow.ch07_jobscope_stepscope.listener.CustomJobListener;
 import com.example.springbatchflow.ch07_jobscope_stepscope.listener.CustomStepListener;
-import javax.batch.api.listener.JobListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -11,8 +10,6 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -21,38 +18,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * JobScope & StepScope
+ * JobScope & StepScope - Architecture
  *
  * <pre>
- *     - Scope
- *          - Spring Container 에서 Bean 이 관리되는 범위
- *          - singleton, prototype, request, session, application 등이 있으며, 기본은 singleton 으로 생성됨
- *
- *     - Spring Batch Scope
- *          - @JobScope, @StepScope
- *              - Job 과 Step 의 Bean 생성과 실행에 관여하는 Scope
- *              - Proxy Mode 를 기본값으로 하는 Scope
+ *     - Proxy 객체 생성
+ *          - @JobScope, @StepScope 가 붙은 Bean 선언은 내부적으로 Bean 의 Proxy 객체가 생성
+ *              - @JobScope
  *                  - @Scope(value = "job", proxyMode = ScopedProxyMode.TARGET_CLASS)
- *              - 해당 Scope 가 선언되면 Bean 이 생성이 Application 구동시점이 아닌 Bean 의 실행시점에 이루어진다.
- *                  - @Values 를 주입해서 Bean 의 실행 시점에 값을 참조할 수 있으며, 일종의 Lazy Binding 이 가능해진다.
- *                  - @Value("#{jobParameters[파라미터명]}"), @Value("#{jobExecutionContext[파라미터명]}"), @Value("#{stepExecutionContext[파라미터명]}")
- *                  - @Values 를 사용할 경우 Bean 선언문에 @JobScope, @StepScope 를 정의하지 않으면 오류를 발생하므로 반드시 선언해야함
- *              - Proxy Mode 로 Bean 이 선언되기 때문에, Application 구동시점에는 Bean 의 Proxy 객체가 생성되어 실행 시점에 실제 Bean 을 호출해준다.
- *              - 병렬처리 시 각 Thread 마다 생성된 Scope Bean 이 할당되기 때문에, Thread 에 안전하게 실행이 가능하다.
+ *              - @StepScope
+ *                  - @Scope(value = "step", proxyMode = ScopedProxyMode.TARGET_CLASS)
  *
- *      - @JobScope
- *          - Step 선언문에 정의한다.
- *          - @Value : jobParameter, JobExecutionContext 만 사용가능
+ *          - Job 실행 시 Proxy 객체가 실제 Bean 을 호출해서 해당 메서드를 실행시키는 구조
  *
- *      - @StepScope
- *          - Tasklet 이나 ItemReader, ItemWriter, itemProcessor 선언문에 정의한다.
- *          - @Value : jobParameter, jobExecutionContext, stepExecutionContext 사용가능
+ *      - JobScope, StepScope
+ *          - Proxy 객체의 실제 대상이 되는 Bean 을 등록, 해제하는 역할
+ *          - 실제 Bean 을 저장하고 있는 JobContext, StepContext 를 가지고 있다.
+ *
+ *      - JobContext, StepContext
+ *          - Spring Container 에서 생성된 Bean 을 저장하는 Context 역할
+ *          - Job 의 실행시점에서 Proxy 객체가 실제 Bean 을 참조할 때 사용됨
+ *
  * </pre>
  */
 @Slf4j
 @RequiredArgsConstructor
-//@Configuration
-public class JobScopeAndStepScopeConfiguration {
+@Configuration
+public class JobScopeAndStepScopeArchitectureConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
