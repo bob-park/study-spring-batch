7259d7976fae8b1ee5c031c804404a43740d2772
Bob Park <hwpark11@kakao.com>
2021-11-20 17:06:23 +0900

    feat : ChunkOrientedTasklet - ChunkProvider / ChunkProcessor

--- spring-batch-chunk/src/main/java/com/example/springbatchchunk/ch01_chunk/ChunkOrientedTaskletConfiguration.java
+++ spring-batch-chunk/src/main/java/com/example/springbatchchunk/ch01_chunk/ChunkOrientedTaskletConfiguration.java
@@ -28,7 +28,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class ChunkOrientedTaskletConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
