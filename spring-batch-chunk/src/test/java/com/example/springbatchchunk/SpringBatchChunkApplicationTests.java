ca680754d70fb1da7c8b5b1ce4f352b32e9ab4e4
Bob Park <hwpark11@kakao.com>
2021-11-20 15:52:03 +0900

    feat : Initialize project

---
+++ spring-batch-chunk/src/test/java/com/example/springbatchchunk/SpringBatchChunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchchunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchChunkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
