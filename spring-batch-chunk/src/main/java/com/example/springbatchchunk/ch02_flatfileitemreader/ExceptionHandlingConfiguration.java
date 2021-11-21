29cd5c93959c4d76fa7f21768a9fd68fafea4b45
Bob Park <hwpark11@kakao.com>
2021-11-21 14:31:50 +0900

    feat : XML StaxEventItemReader - 개념 및 API 설명 + 예제

--- spring-batch-chunk/src/main/java/com/example/springbatchchunk/ch02_flatfileitemreader/ExceptionHandlingConfiguration.java
+++ spring-batch-chunk/src/main/java/com/example/springbatchchunk/ch02_flatfileitemreader/ExceptionHandlingConfiguration.java
@@ -33,7 +33,7 @@ import org.springframework.core.io.ClassPathResource;
  */
 @Slf4j
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class ExceptionHandlingConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
---
+++ spring-batch-chunk/src/main/java/com/example/springbatchchunk/ch03_xmlstaxeventitemreader/model/Customer.java
@@ -0,0 +1,14 @@
+package com.example.springbatchchunk.ch03_xmlstaxeventitemreader.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
