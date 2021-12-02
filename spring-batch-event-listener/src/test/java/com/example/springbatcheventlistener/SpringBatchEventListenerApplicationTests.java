741b5a3c9eb224100c600bd492f83a234333a581
Bob Park <hwpark11@kakao.com>
2021-12-02 21:37:20 +0900

    feat : Initialize project

---
+++ spring-batch-event-listener/src/test/java/com/example/springbatcheventlistener/SpringBatchEventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatcheventlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchEventListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
