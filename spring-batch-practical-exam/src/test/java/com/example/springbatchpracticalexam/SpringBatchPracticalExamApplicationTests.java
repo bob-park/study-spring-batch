59e3633be6c129612080365862e8b5b35d8db873
Bob Park <hwpark11@kakao.com>
2021-12-04 15:27:49 +0900

    feat : Initialize project

---
+++ spring-batch-practical-exam/src/test/java/com/example/springbatchpracticalexam/SpringBatchPracticalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchpracticalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
